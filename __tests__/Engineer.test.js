bdd986ca23b71247a60cc0f7a434d1029ef9b86b
AngerOverApathy <angersavestheworld@gmail.com>
2022-03-03 16:04:17 -0800

    created engineer object

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require('./lib/Engineer');
+
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
+    
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+test('gets engineer github value', () => {
+    const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('gets employee status', () => {
+    const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
+
+    expect(engineer.stateRole()).toEqual("Engineer");
+});
