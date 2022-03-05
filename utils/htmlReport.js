fd8217c022b750c7f0d02ca05e8477c0ed7d7077
AngerOverApathy <angersavestheworld@gmail.com>
2022-03-05 15:20:07 -0800

    Added bootstrap styling

--- utils/htmlReport.js
+++ utils/htmlReport.js
@@ -1,61 +1,74 @@
 const htmlReport = 
 {
 managerProfile: function (manager) {
-
-    console.log('html manager')
     return `
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+                <div class="card text-white bg-primary mb-3" style="max-width: 20rem">
+                    <div class="card-header">Manager</div>
 
-    
-    <div class="col-4 mt-4 card h-100">
-            <div class="card-header">
-                <h3>${manager.name}</h3>
-                <h4>Manager</h4>
-            </div>
-            <div class="card-body">
-                <p class="id">ID: ${manager.id}</p>
-                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office">Office Number: ${manager.officeNumber}</p>
-            </div>
+                        <div class="card-body">
+                        <h4 class="card-title">${manager.name}</h4>
+                        <p class="card-text">
+                        <p class="id">ID: ${manager.id}</p>
+                        <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                        <p class="office">Office Number: ${manager.officeNumber}</p>
+                        </p>
+                        </div>
+                     </div>
+                           
+                </div>
+        </div>
     </div>
     `;
 },
 
 engineerProfile: function (engineer) {
-    console.log('engineer html')
     return `
-    <div class="col-4 mt-4 card h-100">
-            <div class="card-header">
-                <h3>${engineer.name}</h3>
-                <h4>Engineer</h4>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+                <div class="card text-white bg-primary mb-3" style="max-width: 20rem">
+                    <div class="card-header">Engineer</div>
+
+                        <div class="card-body">
+                        <h4 class="card-title">${engineer.name}</h4>
+                        <p class="card-text">
+                        <p class="id">ID: ${engineer.id}</p>
+                        <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                        <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                        </p>
+                        </div>
+                </div>
             </div>
-            <div class="card-body">
-                <p class="id">ID: ${engineer.id}</p>
-                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-                <p class="office">Office Number: ${engineer.officeNumber}</p>
-                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
-            </div>
+        </div>
     </div>
-    `
+    `;
 },
 
 internProfile: function (intern) {
-    console.log('intern html')
     return `
-    <div class="col-4 mt-4 card h-100">
-            <div class="card-header">
-                <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
-            </div>
-            <div class="card-body">
-                <p class="id">ID: ${intern.id}</p>
-                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
-                <p class="office">Office Number: ${intern.officeNumber}</p>
-                <p class="school">School: ${intern.school}</p>
-            </div>
-    </div>
-    `
-}
+<div class="container>
+    <div class="row">
+        <div class="col-sm-4">
+                <div class="card text-white bg-primary mb-3" style="max-width: 20rem">
+                    <div class="card-header">Engineer</div>
 
+                        <div class="card-body">
+                        <h4 class="card-title">${engineer.name}</h4>
+                        <p class="card-text">
+                        <p class="id">ID: ${intern.id}</p>
+                        <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                        <p class="school">School: ${intern.school}</p>
+                        </p>
+                        </div>
+                     </div>             
+             </div>
+        </div>
+    </div>
+    `;
+}
 }
 
 module.exports = htmlReport;
