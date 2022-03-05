be7f2de8cb9adc3fc206dc688190dd2e7c8b29a9
AngerOverApathy <angersavestheworld@gmail.com>
2022-03-05 15:35:41 -0800

    engineer testing complete

--- __tests__/Employees.test.js
+++ __tests__/Employees.test.js
@@ -1,32 +1,27 @@
 const Employee = require('../lib/Employees');
 
 test('creates an employee object', () => {
-
     const employee = new Employee('Mark', 75, 'markymark@gmail.com');
-
     expect(typeof(employee)).toBe('object');
 });
 
 test('gets employee name', () => {
     const testName = 'Mark'
     const employee = new Employee(testName, 75, 'markymark@gmail.com');
-
     expect(employee.name).toEqual(testName);
 });
 
 test('gets employee ID', () => {
     const employee = new Employee('Mark', 75, 'markymark@gmail.com');
-
     expect(employee.idInput()).toEqual(employee.id);
 });
 
 test('gets employee email', () => {
     const employee = new Employee('Mark', 75, 'markymark@gmail.com');
-
     expect(employee.emailInput()).toEqual(employee.email);
 });
 
-// test('gets role of employee', () => {
-//     const employee = new Employee('Mark', 75, 'markymark@gmail.com');
-//     expect(employee.stateRole()).toEqual("Employee");
-// }); 
+test('gets role of employee', () => {
+    const employee = new Employee('Mark', 75, 'markymark@gmail.com');
+    expect(employee.stateRole()).toEqual("Employee");
+}); 
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,19 +1,17 @@
-const Engineer = require('./lib/Engineer');
+const Engineer = require('../lib/Engineer');
 
-// test('creates an Engineer object', () => {
-//     const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
-    
-//     expect(engineer.github) .toEqual(expect.any(String));
-// });
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
+    expect(typeof(engineer)).toBe('object');
+});
 
-// test('gets engineer github value', () => {
-//     const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
+test('gets engineer github value', () => {
+    const githubName = 'webdevjane'
+    const engineer = new Engineer('Jane', 505, 'jane@gmail.com', githubName);
+    expect(engineer.github).toEqual(githubName);
+});
 
-//     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
-// });
-
-// test('gets employee status', () => {
-//     const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
-
-//     expect(engineer.stateRole()).toEqual("Engineer");
-// });
+test('gets employee status', () => {
+    const engineer = new Engineer('Jane', 505, 'jane@gmail.com', 'webdevjane');
+    expect(engineer.stateRole()).toEqual("Engineer");
+});
